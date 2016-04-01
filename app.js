8f52b4707381c0145e42a5fda6f0ae04006d082f
Michael Seid <mbseid@gmail.com>
2016-04-01 10:30:33 -0700

    Adding in the main javascript file

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var browserify = require('browserify-middleware');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+var isDevelopment = app.get('env') === 'development';
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -25,6 +27,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
+app.use('/javascripts/main.js', browserify(__dirname + '/assets/javascripts/main.js'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Redis Configuration Setup
@@ -51,7 +54,7 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (isDevelopment) {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
